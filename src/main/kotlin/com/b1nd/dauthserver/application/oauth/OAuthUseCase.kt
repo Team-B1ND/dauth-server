5ba7d2162a30958d4a4d4f6061f0b33ad97786c8
kmg <legolove08@naver.com>
2025-12-16 22:06:23 +0900

    hotfix: get userinfo error

--- src/main/kotlin/com/b1nd/dauthserver/application/oauth/OAuthUseCase.kt
+++ src/main/kotlin/com/b1nd/dauthserver/application/oauth/OAuthUseCase.kt
@@ -5,7 +5,6 @@ import com.b1nd.dauthserver.application.oauth.data.UserInfoResponse
 import com.b1nd.dauthserver.application.support.response.ResponseData
 import com.b1nd.dauthserver.infrastructure.client.dodam.DodamClient
 import com.b1nd.dauthserver.infrastructure.security.support.UserAuthenticationHolder
-import com.b1nd.dauthserver.infrastructure.security.token.core.TokenProvider
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
@@ -13,21 +12,18 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(rollbackFor = [Exception::class])
 class OAuthUseCase(
     private val dodamClient: DodamClient,
-    private val tokenProvider: TokenProvider
 ) {
     suspend fun getUserInfo(): ResponseData<UserInfoResponse> {
         val user = UserAuthenticationHolder.current()
-        val accessToken = tokenProvider.reissueAccessToken(user.refreshToken)
+        val accessToken = dodamClient.reissueAccessToken(user.refreshToken)
         val memberInfo = dodamClient.dodamMy(accessToken)
         return ResponseData.ok("사용자 정보 조회 성공", UserInfoResponse.of(memberInfo, user.scopes))
     }
 
     suspend fun getStandardUserInfo(): StandardUserInfoResponse {
         val user = UserAuthenticationHolder.current()
-
-        val accessToken = tokenProvider.reissueAccessToken(user.refreshToken)
+        val accessToken = dodamClient.reissueAccessToken(user.refreshToken)
         val memberInfo = dodamClient.dodamMy(accessToken)
-
         return StandardUserInfoResponse.of(memberInfo, user.scopes)
     }
 }
