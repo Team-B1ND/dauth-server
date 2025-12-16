5afce1b78dc00896baa5ae460b3efae016b53211
kmg <legolove08@naver.com>
2025-12-17 00:12:07 +0900

    delete: authentication word

--- src/main/kotlin/com/b1nd/dauthserver/domain/auth/exception/AuthenticationWordExceptions.kt
+++
@@ -1,5 +0,0 @@
-package com.b1nd.dauthserver.domain.auth.exception
-
-import com.b1nd.dauthserver.domain.common.exception.BasicException
-
-class AuthenticationWordNotFoundException : BasicException(AuthenticationWordStatusCode.AUTHENTICATION_WORD_NOT_FOUND)
