393c64fc7de377e15dafd5cb2ccb8d02f64a4cb8
kmg <legolove08@naver.com>
2025-12-21 00:55:51 +0900

    chore: token error

--- src/main/kotlin/com/b1nd/dauthserver/infrastructure/security/filter/FilterExceptionHandler.kt
+++ src/main/kotlin/com/b1nd/dauthserver/infrastructure/security/filter/FilterExceptionHandler.kt
@@ -3,6 +3,8 @@ package com.b1nd.dauthserver.infrastructure.security.filter
 import com.b1nd.dauthserver.application.support.response.ErrorResponse
 import com.b1nd.dauthserver.domain.common.exception.BasicException
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.JwtException
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
@@ -30,7 +32,28 @@ class FilterExceptionHandler(
 
                 val buffer = response.bufferFactory().wrap(bytes)
                 response.writeWith(Mono.just(buffer))
-
             }
+            .onErrorResume(ExpiredJwtException::class.java) { _ ->
+                writeErrorResponse(exchange, HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다")
+            }
+            .onErrorResume(JwtException::class.java) { _ ->
+                writeErrorResponse(exchange, HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다")
+            }
+    }
+
+    private fun writeErrorResponse(
+        exchange: ServerWebExchange,
+        status: HttpStatus,
+        message: String
+    ): Mono<Void> {
+        val response = exchange.response
+        response.statusCode = status
+        response.headers.contentType = MediaType.APPLICATION_JSON
+
+        val body = ErrorResponse(status.value(), message)
+        val bytes = objectMapper.writeValueAsBytes(body)
+
+        val buffer = response.bufferFactory().wrap(bytes)
+        return response.writeWith(Mono.just(buffer))
     }
 }
